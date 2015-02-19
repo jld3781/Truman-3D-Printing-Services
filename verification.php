48d4cf534542126ba019c6c8a29d45114b61d6e3
James Sorsen <jfs1744@vh222004.truman.edu>
2015-02-19 16:17:51 -0600

    Added verification to admin

---
+++ verification.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Jessica, Sam, Jimmy" />
+    <link rel="stylesheet" href="style.css" />
+
+    <title>Verification</title>
+  </head>
+  <body>
+    <?php include( 'nav.html' ); ?>
+    <form method="post" action="admin.php">
+
+      <p>
+        <label for="password">Provide admin password:</label>
+        <input type="text" id="password" name="password" />
+      </p>
+  
+      <p>
+        <input type="submit" value="Enter" name="submit" />
+      </p>
+  
+    </form>
+  </body>
+</html>
