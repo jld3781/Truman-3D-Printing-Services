82dbe7084b1969aacee800ffed288f4cc5c08c28
Samuel Livingston <sjl6333@vh222004.truman.edu>
2015-03-19 15:56:07 -0500

    The verifyLogin page now checks the username and password.

--- verifyLogin.php
+++ verifyLogin.php
@@ -1,25 +1,48 @@
 <?php
-  # Jessica DiMariano
+  # Jessica DiMariano & Samuel Livingston
   error_reporting(E_ALL);
   ini_set('display_errors', '1');
   session_start();
   
   define('USER_DATABASE','users.txt');
-  
-  $lines = file( USER_DATABASE, FILE_IGNORE_NEW_LINES );
+
+  $error_msg = '';
+  $filename = USER_DATABASE;
+  $lines = file( $filename, FILE_IGNORE_NEW_LINES );
   array_shift($lines);
 
-  foreach( $lines as $line ):
-    list( $username, , , , $firstname, $lastname, , $aflag) = explode( "\t", $line );
-    if( $_POST['username'] == trim( $username ) ):
-      $_SESSION['username'] = $_POST['username'];
-      $_SESSION['loggedin'] = true;
-      $_SESSION['firstname'] = $firstname;
-      $_SESSION['lastname'] = $lastname;
-      $_SESSION['aflag'] = $aflag;
-      header( 'Location: home.php' );
+  if( !(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)):
+    if( isset( $_POST['submit'] )):
+      if( isset( $_POST['username'] ) && 
+          preg_match( '|^\w+$|', $_POST['username'] ) &&
+          isset( $_POST['password'] ) && 
+          preg_match( '|^\S+$|', $_POST['password'] )):
+
+        foreach( $lines as $line ):
+          //$oneline = explode( "\t", $line);
+          list( $username, $password, , , $firstname, $lastname, , $aflag) = explode( "\t", $line );
+          //$currentUserName = $oneline[0];
+          //$currentPass = $oneline[1];
+          if( ($_POST['username'] == $username) && 
+            (password_verify( $_POST['password'], $password ))):
+            $_SESSION['username'] =  $currentUserName;
+            $_SESSION['loggedin'] = true;
+            $_SESSION['firstname'] = $firstname;
+            $_SESSION['lastname'] = $lastname;
+            $_SESSION['aflag'] = $aflag;
+            header( 'Location: home.php' );
+          else:
+            header( 'Location: login.php' );
+            $error_msg = 'Username-password pair is invalid';
+          endif;
+        endforeach;
+      else:
+        $error_msg = 'You must enter a valid username-password pair';
+      endif;
     else:
-      header( 'Location: login.php' );
+      header( 'Location: home.php' );
     endif;
-  endforeach;
+  else:
+    header( 'Location: login.php' );
+  endif;
 ?>
