415f07f8dceba50a955bae05ccc40a2122fda78f
James Sorsen <jfs1744@vh222004.truman.edu>
2015-04-19 00:01:55 -0500

    Added sql create document

---
+++ make_3d_printer_tables.sql
@@ -0,0 +1,78 @@
+#Still may need some "Unique" tags candidate keys
+#Has not been run yet!!
+use jfs1744;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE if EXISTS USER;
+DROP TABLE if EXISTS PROJECT;
+DROP TABLE if EXISTS PRINTER;
+DROP TABLE if EXISTS PRINT_JOB;
+DROP TABLE if EXISTS FILAMENT;
+DROP TABLE if EXISTS CAN_PRINT;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+CREATE TABLE USER(
+  Email VARCHAR(255) NOT NULL,
+  FirstName VARCHAR(255) NOT NULL,
+  LastName VARCHAR(255) NOT NULL,
+  PhoneNumber VARCHAR(20) NOT NULL,
+  StudentFlag ENUM('T', 'F') NOT NULL,
+  AdminFlag ENUM('T', 'F') NOT NULL,
+  PasswordHash TEXT NOT NULL, 
+  PRIMARY KEY (Email) );
+  
+CREATE TABLE PROJECT(
+  CreatorEmail VARCHAR(255) NOT NULL,
+  ProjectName VARCHAR(255) NOT NULL,
+  ProjectLink TEXT,
+  Picture LONGBLOB,
+  PRIMARY KEY (CreatorEmail, ProjectName),
+  FOREIGN KEY (CreatorEmail) REFERENCES USER(Email) );
+  
+CREATE TABLE PRINTER(
+  PrinterId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  Location VARCHAR(255) NOT NULL,
+  MaximumLength DECIMAL,
+  MaximumWidth DECIMAL,
+  MaximumHeight DECIMAL,
+  PRIMARY KEY (PrinterId) );
+
+CREATE TABLE FILAMENT(
+  Color VARCHAR(255) NOT NULL,
+  Type VARCHAR(255) NOT NULL,
+  PricePerPound DECIMAL NOT NULL, 
+  PRIMARY KEY (Color, Type) );
+  
+CREATE TABLE CAN_PRINT(
+  Color VARCHAR(255) NOT NULL,
+  Type VARCHAR(255) NOT NULL,
+  PrinterId INTEGER UNSIGNED NOT NULL,
+  PRIMARY KEY (Color, Type, PrinterId)
+  FOREIGN KEY (Color, Type) REFERENCES FILAMENT(Color, Type),
+  FOREIGN KEY (PrinterId) REFERENCES PRINTER(PrinterId) );
+
+CREATE TABLE PRINT_JOB(
+  JobId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  PrinterId INTEGER UNSIGNED NOT NULL,
+  PrinterEmail VARCHAR(255) NOT NULL,
+  CreatorEmail VARCHAR(255) NOT NULL,
+  ProjectName VARCHAR(255) NOT NULL,
+  Status ENUM('Waiting', 'Printing', 'Stopped', 'Complete') NOT NULL,
+  StartTime DATETIME,
+  StopTime DATETIME,
+  Length DECIMAL NOT NULL,
+  Width DECIMAL NOT NULL,
+  Height DECIMAL NOT NULL,
+  Color VARCHAR(255) NOT NULL,
+  ChargedPrice DECIMAL,
+  Comment TEXT,
+  Weight DECIMAL NOT NULL,
+  MaterialType VARCHAR(255) NOT NULL,
+  PRIMARY KEY (JobId),
+  FOREIGN KEY (Color, MaterialType, PrinterId) REFERENCES CAN_PRINT(Color, Type, PrinterId),
+  FOREIGN KEY (PrinterEmail) REFERENCES EMPLOYEE(Email),
+  FOREIGN KEY (CreatorEmail, ProjectName) REFERENCES PROJECT(CreatorEmail, ProjectName) );
+
