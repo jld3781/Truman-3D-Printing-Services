d1d653fd862dda7e938f8cac3757565acca70abb
Samuel Livingston <sjl6333@vh222004.truman.edu>
2015-04-19 13:01:31 -0500

    Working table creator & table populator

--- make_3d_printer_tables.sql
+++ make_3d_printer_tables.sql
@@ -1,6 +1,6 @@
 #Still may need some "Unique" tags candidate keys
 #Has not been run yet!!
-use jfs1744;
+use sjl6333;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -12,7 +12,6 @@ DROP TABLE if EXISTS FILAMENT;
 DROP TABLE if EXISTS CAN_PRINT;
 
 SET FOREIGN_KEY_CHECKS = 1;
-
 
 CREATE TABLE USER(
   Email VARCHAR(255) NOT NULL,
@@ -50,7 +49,7 @@ CREATE TABLE CAN_PRINT(
   Color VARCHAR(255) NOT NULL,
   Type VARCHAR(255) NOT NULL,
   PrinterId INTEGER UNSIGNED NOT NULL,
-  PRIMARY KEY (Color, Type, PrinterId)
+  PRIMARY KEY (Color, Type, PrinterId),
   FOREIGN KEY (Color, Type) REFERENCES FILAMENT(Color, Type),
   FOREIGN KEY (PrinterId) REFERENCES PRINTER(PrinterId) );
 
@@ -60,19 +59,21 @@ CREATE TABLE PRINT_JOB(
   PrinterEmail VARCHAR(255) NOT NULL,
   CreatorEmail VARCHAR(255) NOT NULL,
   ProjectName VARCHAR(255) NOT NULL,
-  Status ENUM('Waiting', 'Printing', 'Stopped', 'Complete') NOT NULL,
+  Status ENUM('Waiting', 'Printing', 'On Hold', 'Complete', 'Rejected') NOT NULL,
   StartTime DATETIME,
   StopTime DATETIME,
-  Length DECIMAL NOT NULL,
-  Width DECIMAL NOT NULL,
-  Height DECIMAL NOT NULL,
+  Length DECIMAL,
+  Width DECIMAL,
+  Height DECIMAL,
   Color VARCHAR(255) NOT NULL,
   ChargedPrice DECIMAL,
   Comment TEXT,
-  Weight DECIMAL NOT NULL,
+  Weight DECIMAL,
   MaterialType VARCHAR(255) NOT NULL,
   PRIMARY KEY (JobId),
   FOREIGN KEY (Color, MaterialType, PrinterId) REFERENCES CAN_PRINT(Color, Type, PrinterId),
-  FOREIGN KEY (PrinterEmail) REFERENCES EMPLOYEE(Email),
+  FOREIGN KEY (PrinterEmail) REFERENCES USER(Email),
   FOREIGN KEY (CreatorEmail, ProjectName) REFERENCES PROJECT(CreatorEmail, ProjectName) );
+
+insert into USER(Email, FirstName, LastName, PhoneNumber, StudentFlag, AdminFlag, PasswordHash) VALUES('hello@gmail.com', 'Samuel', 'Livingston', '8165880680', 'T', 'T', '$osiehjf9pa8w37r893nhfui3y287');
 
