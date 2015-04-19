e535ad49822965cf78d7238f9c739eb3826abca0
Jessica DiMariano <jld3781@truman.edu>
2015-04-19 14:06:17 -0500

    Created database connection file

---
+++ dbconnection.php
@@ -0,0 +1,10 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', '1');
+  require_once( '../../cs315/dblogin.php' );
+  $db = new PDO(
+  "mysql:host=$db_hostname;dbname=sjl6333;charset=utf8",
+  $db_username, $db_password,
+  array(PDO::ATTR_EMULATE_PREPARES => false,
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+?>
